aca902b0fff060ee94fea46cd413705b5cdc3abd
o.bakhur <o.bakhur@vironit.co.uk>
2024-07-22 15:43:47 +0200

    add BatteryManager

--- composeApp/src/androidMain/kotlin/org/kmp/sample/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/org/kmp/sample/MainActivity.kt
@@ -1,24 +1,22 @@
 package org.kmp.sample
 
 import App
+import BatteryManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.remember
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            App()
+            App(
+                batteryManager = remember {
+                    BatteryManager(applicationContext)
+                }
+            )
         }
     }
-}
-
-@Preview
-@Composable
-fun AppAndroidPreview() {
-    App()
 }
