ae68722c875662602e2d8e52d83cdeb634c323d3
o.bakhur <o.bakhur@vironit.co.uk>
2024-07-23 17:24:16 +0200

    add okhttpClient

--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -1,6 +1,10 @@
+import androidx.compose.runtime.remember
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import di.initKoin
+import io.ktor.client.engine.okhttp.OkHttp
+import networking.InsultCensorClient
+import networking.createHttpClient
 
 fun main() {
     initKoin()
@@ -10,7 +14,11 @@ fun main() {
             onCloseRequest = ::exitApplication,
             title = "KMPSample",
         ) {
-            App()
+            App(
+                client = remember {
+                    InsultCensorClient(createHttpClient(OkHttp.create()))
+                }
+            )
         }
     }
 }
--- composeApp/src/iosMain/kotlin/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/MainViewController.kt
@@ -1,10 +1,18 @@
+import androidx.compose.runtime.remember
 import androidx.compose.ui.window.ComposeUIViewController
 import di.initKoin
+import io.ktor.client.engine.darwin.Darwin
+import networking.InsultCensorClient
+import networking.createHttpClient
 
 fun MainViewController() = ComposeUIViewController(
     configure = {
         initKoin()
     }
 ) {
-    App()
+    App(
+        client = remember {
+            InsultCensorClient(createHttpClient(Darwin.create()))
+        }
+    )
 }
