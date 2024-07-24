4269531938d9d6c5fb4e875f7e8f755333c5a0a9
o.bakhur <o.bakhur@vironit.co.uk>
2024-07-24 16:03:48 +0200

    add permissions dependencies

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,33 +1,64 @@
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import dev.icerock.moko.permissions.PermissionState
+import dev.icerock.moko.permissions.compose.BindEffect
+import dev.icerock.moko.permissions.compose.rememberPermissionsControllerFactory
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import kmpsample.composeapp.generated.resources.Res
-import kmpsample.composeapp.generated.resources.compose_multiplatform
+import viewModel.PermissionsViewModel
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
+        val factory = rememberPermissionsControllerFactory()
+        val controller = remember(factory) {
+            factory.createPermissionsController()
+        }
+
+        BindEffect(controller)
+
+        val viewModel = viewModel {
+            PermissionsViewModel(controller)
+        }
+
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            when (viewModel.state) {
+                PermissionState.Granted -> {
+                    Text("Record audio permission granted!")
+                }
+
+                PermissionState.DeniedAlways -> {
+                    Text("Permission was permanently denied.")
+                    Button(
+                        onClick = {
+                            controller.openAppSettings()
+                        }
+                    ) {
+                        Text("Open App settings")
+                    }
+                }
+
+                else -> {
+                    Button(
+                        onClick = {
+                            viewModel.provideOrRequestRecordAudioPermission()
+                        }
+                    ) {
+                        Text("Request permission")
+                    }
                 }
             }
         }
---
+++ composeApp/src/commonMain/kotlin/viewModel/PermissionsViewModel.kt
@@ -0,0 +1,43 @@
+package viewModel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dev.icerock.moko.permissions.DeniedAlwaysException
+import dev.icerock.moko.permissions.DeniedException
+import dev.icerock.moko.permissions.Permission
+import dev.icerock.moko.permissions.PermissionState
+import dev.icerock.moko.permissions.PermissionsController
+import dev.icerock.moko.permissions.RequestCanceledException
+import kotlinx.coroutines.launch
+
+class PermissionsViewModel(
+    private val permissionsController: PermissionsController
+) : ViewModel() {
+
+    var state by mutableStateOf(PermissionState.NotDetermined)
+        private set
+
+    init {
+        viewModelScope.launch {
+            state = permissionsController.getPermissionState(Permission.RECORD_AUDIO)
+        }
+    }
+
+    fun provideOrRequestRecordAudioPermission() {
+        viewModelScope.launch {
+            try {
+                permissionsController.providePermission(Permission.RECORD_AUDIO)
+                state = PermissionState.Granted
+            } catch (e: DeniedAlwaysException) {
+                state = PermissionState.DeniedAlways
+            } catch (e: DeniedException) {
+                state = PermissionState.Denied
+            } catch (e: RequestCanceledException) {
+                e.printStackTrace()
+            }
+        }
+    }
+}
